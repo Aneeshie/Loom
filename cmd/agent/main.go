b086f67bff3ad46059cc5860f4828f7b29b8ebbd
Aneeshie <aneeshdas556@gmail.com>
2026-06-12 17:48:39 +0530

    feat: read logs from file and ship via grpc

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"log"
+	"os"
+	"strings"
+	"time"
 
 	pb "github.com/Aneeshie/loom/proto"
 
@@ -11,31 +15,80 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 )
 
+// for now
+const (
+	serviceName = "test_service"
+	host        = "localhost"
+)
+
 func main() {
 	var opts []grpc.DialOption
 	opts = append(opts, grpc.WithTransportCredentials(
 		insecure.NewCredentials(),
 	))
-	
+
 	conn, err := grpc.NewClient(":8080", opts...)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer conn.Close()
 
-	client := pb.NewLogServiceClient(conn)
-	resp, err := client.SendLog(context.Background(), &pb.SendLogRequest{
-		ServiceName: "test",
-		Host:        "ubuntu",
-		Level:       "INFO",
-		Message:     "A test Log",
-		Timestamp:   12321,
-	})
+	//TODO: move log file path to config
+	file, err := os.Open("../../testdata/test.log")
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(resp.Message)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	client := pb.NewLogServiceClient(conn)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		level, message, err := parseLogLine(line)
+		if err != nil {
+			log.Printf("failed to send log: %v", err)
+			continue
+		}
+
+		resp, err := client.SendLog(context.Background(), &pb.SendLogRequest{
+			ServiceName: serviceName,
+			Host:        host,
+			Level:       level,
+			Message:     message,
+			Timestamp:   time.Now().Unix(),
+		})
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(resp.Message)
+
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func parseLogLine(logLine string) (string, string, error) {
+
+	start := strings.Index(logLine, "[")
+	end := strings.Index(logLine, "]")
+
+	if start != -1 && end != -1 && end > start {
+		level := logLine[start+1 : end]
+
+		message := strings.TrimSpace(logLine[end+1:])
+
+		return level, message, nil
+
+	}
+	return "", "", fmt.Errorf("invalid format")
 
 }
